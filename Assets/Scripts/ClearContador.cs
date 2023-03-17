fe214cf6ae358a031629e3a25fda30289e5e2300
amna <antoniomiguel2001@hotmail.es>
2023-03-17 16:21:50 +0100

    joya object parent desarrollado

--- Assets/Scripts/ClearContador.cs
+++ Assets/Scripts/ClearContador.cs
@@ -3,15 +3,52 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class ClearContador : MonoBehaviour
-{
-    [SerializeField] private ObjetoInteractuableSO objetoInteractuable
-        ;
+public class ClearContador : MonoBehaviour {
+    [SerializeField] private ObjetoInteractuableSO objetoInteractuable;
     [SerializeField] private Transform puntoSuperiorObjeto;
-    public void Interactuar() {
-       Transform ingredienteTransform =  Instantiate(objetoInteractuable.prefab, puntoSuperiorObjeto);
-        ingredienteTransform.localPosition = Vector3.zero;
+    [SerializeField] private ClearContador testContador;
+    [SerializeField] private bool testBool;
+    private ObjetoInteractuable objInteractuable;
 
-        Debug.Log(ingredienteTransform.GetComponent<ObjetoInteractuable>().GetObjetoInteractuableSO().nombreObjeto);
+
+    private void Update() {
+        if (testBool && Input.GetKeyDown(KeyCode.T)) {
+            if (objInteractuable != null) {
+                objInteractuable.SetClearContador(testContador);
+            }
+        }
+    }
+
+    public void Interactuar() {
+        //Condicional para la antiduplicación de objetos (ingredientes)
+        if (objInteractuable == null) {
+            Transform ingredienteTransform = Instantiate(objetoInteractuable.prefab, puntoSuperiorObjeto);
+            ingredienteTransform.GetComponent<ObjetoInteractuable>().SetClearContador(this);
+            
+        } else {
+            //Esta condición se dará cuando ya exista un objeto encima del objeto
+            Debug.Log(objInteractuable.GetClearContador());
+        }
+    }
+
+    public Transform GetObjetoInteractuableConTransform() {
+        return puntoSuperiorObjeto;
+    }
+
+
+    public ObjetoInteractuable GetObjetoInteractuable() {
+        return objInteractuable;
+    }
+
+    public void SetObjInteractuable(ObjetoInteractuable objInteractuable) {
+        this.objInteractuable = objInteractuable;
+    }
+
+    public void ClearObjInteractuable() {
+        objInteractuable = null;
+    }
+
+    public bool objInteractuableActivo() { 
+        return objInteractuable != null;
     }
 }
--- Assets/Scripts/ObjetoInteractuable.cs
+++ Assets/Scripts/ObjetoInteractuable.cs
@@ -2,13 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObjetoInteractuable : MonoBehaviour
-{
+public class ObjetoInteractuable : MonoBehaviour {
     [SerializeField] private ObjetoInteractuableSO objetoInteractuableSO;
+    private ClearContador clearContador;
+
     /**
      * Método para obtener el objeto interactuable
      */
     public ObjetoInteractuableSO GetObjetoInteractuableSO() {
         return objetoInteractuableSO;
     }
+
+    /**
+     * Cuando el objeto interactuable se cambia de objeto padre, también cambia su posición
+     * a partir de la siguiente función
+     */
+    public void SetClearContador(ClearContador clearContador) {
+        if (this.clearContador != null) {
+            this.clearContador.ClearObjInteractuable();
+        }
+        this.clearContador = clearContador;
+
+        if (clearContador.objInteractuableActivo()) {
+            Debug.LogError("Ya tiene un ingrediente el objeto padre / encimera");
+        }
+        clearContador.SetObjInteractuable(this);
+
+        transform.parent = clearContador.GetObjetoInteractuableConTransform();
+        transform.localPosition = Vector3.zero;
+    }
+
+
+
+    public ClearContador GetClearContador() {
+        return clearContador;
+    }
+
 }
